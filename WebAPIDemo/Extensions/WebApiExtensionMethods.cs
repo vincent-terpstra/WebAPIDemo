580519f6136367a80c512e8e19d1d8d68b3802b4
Vincent Terpstra <vdterpst@gmail.com>
2023-03-08 20:02:10 -0500

    Attempt to switch from InMem to Sqlite db

--- WebAPIDemo/Extensions/WebApiExtensionMethods.cs
+++ WebAPIDemo/Extensions/WebApiExtensionMethods.cs
@@ -24,6 +24,7 @@ public static class WebApiExtensionMethods
         );
 
         builder.Services.AddDbContext<AppDbContext>(
+            //opt => opt.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase"))
             opt => opt.UseInMemoryDatabase("InMem")
         );
 
--- WebAPIDemo/Program.cs
+++ WebAPIDemo/Program.cs
@@ -1,52 +1,43 @@
 using Application.Abstractions;
 using DataAccess;
-using MinimalAPIDemo;
 using WebAPIDemo.Extensions;
 using WebAPIDemo.Filters;
 
-namespace WebAPIDemo;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
+builder.Services.AddControllers();
 
-        var builder = WebApplication.CreateBuilder(args);
+builder.RegisterServices();
 
-        builder.Services.AddControllers();
-        
-        builder.RegisterServices();
-        
-        var app = builder.Build();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-            var scope = app.Services.CreateScope();
-            var users = scope.ServiceProvider.GetRequiredService<IUserService>();
-            users.PopulateDbUsers();
+    var scope = app.Services.CreateScope();
+    var users = scope.ServiceProvider.GetRequiredService<IUserService>();
+    users.PopulateDbUsers();
 
-            var posts = scope.ServiceProvider.GetRequiredService<IPostRepository>();
-            posts.PopulateDbPosts();
-
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-        
-        app.RegisterExceptionHandling();
-        
-        app.UseMiddleware<ExceptionHandlerMiddleware>();
-        app.UseMiddleware<KeyNotFoundMiddleware>();
-        
-        app.MapControllers();
-        
-        app.RegisterEndpoints();
-
-        app.Run();
-    }
+    var posts = scope.ServiceProvider.GetRequiredService<IPostRepository>();
+    posts.PopulateDbPosts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.RegisterExceptionHandling();
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+app.UseMiddleware<KeyNotFoundMiddleware>();
+
+app.MapControllers();
+
+app.RegisterEndpoints();
+
+app.Run();
+
+public partial class Program {}
